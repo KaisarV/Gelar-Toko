44a23d3f33e726031d4682bc1551251c8a945472
KaisarV <kaisar.valentino123@gmail.com>
2022-04-10 13:57:53 +0700

    update store

--- controllers/store_controller.go
+++ controllers/store_controller.go
@@ -57,7 +57,7 @@ func DeleteMyStore(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	var response model.ErrorResponse
-	_, userId, _, _ := validateTokenFromCookies(r)
+	_, userId, userName, _ := validateTokenFromCookies(r)
 
 	query, errQuery := db.Exec(`DELETE FROM stores WHERE User_Id = ?;`, userId)
 	RowsAffected, _ := query.RowsAffected()
@@ -74,6 +74,7 @@ func DeleteMyStore(w http.ResponseWriter, r *http.Request) {
 		query, errQuery = db.Exec("UPDATE users SET User_Type = ? WHERE Id = ?", 1, userId)
 		response.Status = 200
 		response.Message = "Success Delete Data"
+		generateToken(w, userId, userName, 1)
 	} else {
 		response.Status = 400
 		response.Message = "Error Delete Data"
@@ -97,6 +98,16 @@ func InsertMyStore(w http.ResponseWriter, r *http.Request) {
 		response.Status = 400
 		response.Message = "You already have a store"
 		SendResponse(w, response.Status, response)
+		return
+	}
+
+	err := r.ParseForm()
+
+	if err != nil {
+		response.Status = 400
+		response.Message = "Error Parsing Data"
+		w.WriteHeader(400)
+		log.Println(err.Error())
 		return
 	}
 
@@ -135,6 +146,68 @@ func InsertMyStore(w http.ResponseWriter, r *http.Request) {
 		response.Status = 400
 		response.Message = "Error Insert Data"
 		log.Println(errQuery.Error())
+	}
+	SendResponse(w, response.Status, response)
+}
+
+func UpdateMyStore(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+
+	var store model.Store
+	var response model.StoreResponse
+	err := r.ParseForm()
+
+	if err != nil {
+		response.Status = 400
+		response.Message = "Error Parsing Data"
+		w.WriteHeader(400)
+		log.Println(err.Error())
+		return
+	}
+
+	store.Name = r.Form.Get("name")
+	store.Address = r.Form.Get("address")
+
+	_, userId, _, _ := validateTokenFromCookies(r)
+	// store.Name = input.Name
+	// store.Address = input.Address
+
+	rows, _ := db.Query("SELECT * FROM stores WHERE User_Id = ?", userId)
+	var prevDatas []model.Store
+	var prevData model.Store
+
+	for rows.Next() {
+		if err := rows.Scan(&prevData.ID, &prevData.UserId, &prevData.Name, &prevData.Address); err != nil {
+			log.Println(err.Error())
+		} else {
+			prevDatas = append(prevDatas, prevData)
+		}
+	}
+
+	if len(prevDatas) > 0 {
+		if store.Name == "" {
+			store.Name = prevDatas[0].Name
+		}
+		if store.Address == "" {
+			store.Address = prevDatas[0].Address
+		}
+
+		_, errQuery := db.Exec(`UPDATE stores SET Name = ?,  Address = ? WHERE User_id = ?`, store.Name, store.Address, userId)
+
+		if errQuery == nil {
+			response.Status = 200
+			response.Message = "Success Update Data"
+			store.ID = prevData.ID
+			response.Data = store
+		} else {
+			response.Status = 400
+			response.Message = "Error Update Data"
+
+			log.Println(errQuery)
+		}
+	} else {
+		response.Status = 400
+		response.Message = "Data Not Found"
 	}
 	SendResponse(w, response.Status, response)
 }
