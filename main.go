6b25e1472119bf3697f1a41d78f58ebae53f0314
KaisarV <kaisar.valentino123@gmail.com>
2022-04-10 10:41:05 +0700

    cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -14,7 +15,15 @@ func main() {
 	router.HandleFunc("/login", controller.UserLogin).Methods("POST")
 	router.HandleFunc("/logout", controller.Logout).Methods("GET")
 
-	err := http.ListenAndServe(":8080", router)
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+	})
+	handler := corsHandler.Handler(router)
+	log.Println("Starting on Port")
+
+	err := http.ListenAndServe(":8080", handler)
 	log.Fatal(err)
 
 }
